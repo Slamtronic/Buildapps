64f9170393aeb592251a3adce72be05a58fcefff
Slamtronic <139569102+Slamtronic@users.noreply.github.com>
2024-09-30 16:36:41 +0100

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Demo{
+	
+	public static void main(String[] args){
+		System.out.println("          ######################################\n          ####this the first project........####\n          ######################################");
+	}
+	
+	
+}
